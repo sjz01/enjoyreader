3c6a21a8f455c0aee9f7477c2ce5a8c880142e46
zqy979 <zhangqianyao10@163.com>
2019-07-26 15:04:42 +0800

    bookshelf的页面和布局

--- src/views/BookShelf.vue
+++ src/views/BookShelf.vue
@@ -1,6 +1,38 @@
 <template>
     <div id="book-shelf">
         <!-- 书架 -->
+        <div class='bookone'>
+            <ul class='books'>
+            <li>
+                <img src="http://img0.imgtn.bdimg.com/it/u=3963626460,748883260&fm=26&gp=0.jpg" alt="">
+                <p>小公司小公gcgjjjjjjjjjjjjjjjjjjjjjjjjc</p>
+            </li>
+            <li>
+                <img src="http://img0.imgtn.bdimg.com/it/u=3963626460,748883260&fm=26&gp=0.jpg" alt="">
+                <p>书嘻哈帮</p>
+            </li>
+            <li>
+                <img src="http://img0.imgtn.bdimg.com/it/u=3963626460,748883260&fm=26&gp=0.jpg" alt="">
+                <p>书</p>
+            </li>
+            <li>
+                <img src="http://img0.imgtn.bdimg.com/it/u=3963626460,748883260&fm=26&gp=0.jpg" alt="">
+                <p>书</p>
+            </li>
+            <li>
+                <img src="http://img0.imgtn.bdimg.com/it/u=3963626460,748883260&fm=26&gp=0.jpg" alt="">
+                <p>书</p>
+            </li>
+            <li>
+                <img src="http://img0.imgtn.bdimg.com/it/u=3963626460,748883260&fm=26&gp=0.jpg" alt="">
+                <p>书</p>              
+            </li>
+             <li>
+                <img src="http://img0.imgtn.bdimg.com/it/u=2417024038,232503512&fm=26&gp=0.jpg" alt="">             
+            </li>
+        </ul>  
+        </div>
+        
         <p>我是书架</p>  
     
     </div>
@@ -12,10 +44,43 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scoped>
 #book-shelf{
-    margin-top: 100px;
-    background: #eee;
+     margin-top: 60px;
+     width: 100%;
 }
+.bookone{
+    width: 100%;
+}
+.books{
+      width: 94%;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      overflow: hidden;
+      align-items: center;
+      margin: 0 auto;
+
+}
+li{
+    width: 26%;
+    height: 100%;
+    // box-sizing: border-box;
+}
+li img{
+    width: 80px;
+    height: 120px;
+    border-radius:10px; 
+    text-align: center;
+}
+li p{
+    text-align: center;
+    font-size: 15px;
+    height: 50px;    
+     overflow: hidden;
+     text-overflow: ellipsis;
+}
+
+
 
 </style>
