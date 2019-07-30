da8c8eb0d3821144ab4ea61e8ada9db5d337c63d
sufficent <wlkpco@163.com>
2019-07-30 22:13:24 +0800

    整理了下注册

--- src/axios/myApi.js
+++ src/axios/myApi.js
@@ -3,13 +3,18 @@ const BOOKURL = "/api/data"
 const MYKEY = "XOjC/NViS4/+ipyL84oxT2zBOQTgsJeZ/px06Q";
 const RN = 20;
 
+
+//登录注册常量
+const TLJYURL = 'http://123.56.195.82:9999'
+const REGISTER_PATH = '/register'
+const LOGIN_PATH = '/login'
+const FIND_PATH = '/existUser'
+const CORRCET_PATH = '/getInfo'
+const PASSWORD_PATH='/updatePassword'
+
 //书籍类别
 const CATALOG_ID = {
-    CHINA_CULTURE : 242,
-    A:243,
-    B:244,
-    C:245,
-    D:246
+    CHINA_CULTURE : 242
 }
 
  //get总方法
@@ -27,6 +32,45 @@ function myGet(vue,data) {
     return vue.axios.get(BOOKURL + "?" + str)
 }
 
+//post 登录注册方法
+function register(vue,username,password,question,answer) {
+    var param = new URLSearchParams();
+    param.append("userName", username);
+    param.append("password", password);
+    param.append("question", question);
+    param.append("answer", answer);
+    return vue.$axios.post(TLJYURL+REGISTER_PATH,param)
+}
+
+function login(vue,username,password) {
+    var param = new URLSearchParams();
+    param.append("userName", username);
+    param.append("password", password);
+    return vue.$axios.post(TLJYURL+LOGIN_PATH,param)
+}
+
+
+function find(vue,username) {
+    var param = new URLSearchParams();
+    param.append("userName", username);
+    return vue.$axios.post(TLJYURL+FIND_PATH,param) 
+}
+
+function correct(vue,username) {
+    var param = new URLSearchParams();
+    param.append("userName", username);
+    return vue.$axios.post(TLJYURL+CORRCET_PATH,param)
+}
+
+function update(vue,username,password,question,answer) {
+    var param = new URLSearchParams();
+    param.append("userName", username);
+    param.append("password", password);
+    param.append("question", question);
+    param.append("answer", answer);
+    return vue.$axios.post(TLJYURL+PASSWORD_PATH,param)
+}
+
 //暴露方法
 function getBookData(vue,data) {
     return myGet(vue,data);
@@ -34,6 +78,6 @@ function getBookData(vue,data) {
 
 
 export default {
-    getBookData
+    getBookData,register,login,find,correct,update
 }
 
