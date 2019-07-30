0270935fb39675ae5cfdd2f557ad53781d3cfe03
sufficent <wlkpco@163.com>
2019-07-30 14:46:28 +0800

    type可以了

--- src/components/Type.vue
+++ src/components/Type.vue
@@ -2,15 +2,16 @@
   <div id="type">
     <!-- 只是组件，不是路由 -->
     <!-- 图书分类 中包括book图书样本 和 more一个分类的全部图书  -->
-    <div class="typeTop" v-for="(result,key) in result" :key="key">
-      <span>{{result.catalog}}</span>
+    <div class="typeTop" >
+      <!-- <span>{{mydata.catalog}}</span> -->
+      <span>中国文学</span>
       <router-link to="/more">
         <span @click="changeTitle('书城')" style="float:right;margin-right:14px;color:black; font-size: 16px;font-weight:400;">更多></span>
       </router-link>
-      <ul class="typeBook"  v-for="(mydata,key) in mydata" :key="key">
+      <ul class="typeBook">
         <router-link to="/bookdetail">
-          <li @click="changeTitle('书城')">
-            <img :src=mydata.img>
+          <li @click="changeTitle('书城')"  v-for="(mydata,key) in mydata" :key="key">
+            <img :src=mydata.img >
           </li>
         </router-link>
       </ul>
@@ -36,15 +37,15 @@ export default {
   },
   created(){
     // 发起请求
-    this.$axios.get("api/goodbook/catalog?key=b72dfbf2e7198d964adb73307fe02443")
-    .then((res)=>{
-      console.log(res);
-      this.result = res.data.result;
-      // console.log(this.result)
-      // console.log(mysrc)
-    })
+    // this.$axios.get("api/goodbook/catalog?key=b72dfbf2e7198d964adb73307fe02443")
+    // .then((res)=>{
+    //   console.log(res);
+    //   this.result = res.data.result;
+    //   // console.log(this.result)
+    //   // console.log(mysrc)
+    // })
     // http://apis.juhe.cn/goodbook/query?key=b72dfbf2e7198d964adb73307fe02443&catalog_id=246&rn=10&rn=10
-    this.$axios.get("api/goodbook/query?key=b72dfbf2e7198d964adb73307fe02443&catalog_id=246&rn=0&rn=3") 
+    this.$axios.get("api/data/?id=64&key=AeHG+dFkG93+ip2L8Yo2QGzDMwTgsJeZ/px06Q&catalog_id=242&pn=10&rn=3") 
     .then((res)=>{
       console.log(res);
        this.mydata = res.data.result.data;
@@ -53,7 +54,7 @@ export default {
 }
 };
 </script>
-<style scoped>
+<style scoped  lang="less">
 #type {
   margin-top: 20px;
 }
@@ -89,26 +90,30 @@ span button {
   box-shadow: 2px 2px 5px #333;
   /* float: left; */
   /* display: inline-block; */
+  
  
 }
-/* .typeBook :nth-of-type(2) {
-  height: 120px;
-  width: 80px;
-  background: #8a8a8a;
-  border-radius: 10px;
-  box-shadow: 2px 2px 5px #333;
-}
-.typeBook :nth-of-type(3) {
-  height: 120px;
-  width: 80px;
-  background: #8a8a8a;
-  border-radius: 10px;
-  box-shadow: 2px 2px 5px #333;
-} */
 .e4e6f1{
   margin-top: 20px;
   height: 20px;
   width: 100%;
   background-color: #e4e6f1;
+}
+.typeBook{
+  // float: left;
+  display: flex;
+  justify-content: space-around;
+  margin-right: 30px;
+  li{
+    width: 20%;
+    height: 30%;
+      float: left;
+      margin-left: 40px;
+      img{
+        width: 80px;
+        height: 120px;
+
+      }
+  }
 }
 </style>
